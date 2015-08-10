1979dc84776698c4185d29e0b594fd83881d1d5e
Yelena Nedelko & Andrew Brennwald <student@epicodus.com>
2015-08-10 11:41:50 -0700

    Add passing spec and code for equilateral triangles.

--- lib/triangles.rb
+++ lib/triangles.rb
@@ -7,7 +7,20 @@ class Triangle
     @triarray.sort!()
   end
 
+## Triangle Legend
+#  1 = Equilateral, 2 = Isosceles, 3 = Scalene
+
   define_method(:triangular?) do
-    (@triarray[0] + @triarray [1]).>=(@triarray[2])
+    if (@triarray[0] + @triarray [1]).>=(@triarray[2])
+      if @triarray.uniq().count() == 1
+        1
+      elsif
+        2
+      else
+        3
+      end
+    else
+      false
+    end
   end
 end
--- spec/triangles_spec.rb
+++ spec/triangles_spec.rb
@@ -7,5 +7,9 @@ describe(Triangle) do
       test_triangle = Triangle.new(2, 2, 8)
       expect(test_triangle.triangular?()).to(eq(false))
     end
+    it("return 1 if it is an equilateral") do
+      test_triangle = Triangle.new(3, 3, 3)
+      expect(test_triangle.triangular?()).to(eq(1))
+    end
   end
 end
